92d9bd0e6fe2ea4aa9bebf7142b53f8c5c3c3ed0
Alexander <Alexander-e-mail@yandex.ru>
2019-10-02 18:31:02 +0300

    UnitTesting add

---
+++ UnitTesting/Calculator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTesting
+{
+    public class Calculator
+    {
+        public long Add(int a, int b)
+        {
+            return (long)a + (long)b;
+        }
+
+        public long Sub(int a, int b)
+        {
+            return (long)a - (long)b;
+        }
+
+        public long Mul(int a, int b)
+        {
+            return (long)a * (long)b;
+        }
+
+        public long Div(int a, int b)
+        {
+            if (b != 0)
+            {
+                return (long)a / (long)b;
+            }
+            else
+            {
+                return long.MinValue;
+            }
+        }
+
+        public long Mod(int a, int b)
+        {
+            if (b != 0)
+            {
+                return (long)a % (long)b;
+            }
+            else
+            {
+                return long.MinValue;
+            }
+        }
+    }
+}
